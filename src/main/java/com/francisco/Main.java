b91d77b96cf03c7cbc85dcc475fc8946ca104519
Francisco Colmenero <franciscojcolmenero@outlook.com>
2025-12-02 14:11:28 -0600

    Basic maven structure

--- src/main/java/com/francisco/Main.java
+++ src/main/java/com/francisco/Main.java
@@ -2,6 +2,6 @@ package com.francisco;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Basic Management System");
     }
 }
