c2405e25e17cda3c0caa7633cf8f563ff21ba815
Francisco Colmenero <franciscojcolmenero@outlook.com>
2025-12-03 09:41:30 -0600

    Basic save into json

---
+++ src/main/java/com/francisco/FileManager.java
@@ -0,0 +1,44 @@
+package com.francisco;
+
+import com.google.gson.Gson;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileManager {
+
+    private Gson gson;
+    private ParkingLot parkingLot;
+
+    FileManager(){
+        gson = new Gson();
+    }
+
+    public ParkingLot getParkingLot(String fileName){
+        try (FileReader fileReader = new FileReader(fileName)) {
+            System.out.println("Parking lot loaded from " + fileName);
+            System.out.println();
+            parkingLot = gson.fromJson(fileReader, ParkingLot.class);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found. " + fileName);
+            parkingLot = null;
+        }
+        catch (IOException e){
+            System.out.println("Error reading file." + e.getMessage());
+            parkingLot = null;
+        }
+        return parkingLot;
+    }
+
+    public void saveParkingLot(String fileName, ParkingLot parkingLot){
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
+            String json = gson.toJson(parkingLot);
+        }
+        catch(IOException e){
+            System.out.println("Error writing to file." + e.getMessage());
+        }
+    }
+}
